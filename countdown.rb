9f5bbc5f531e4f270ca14a3be6193f31737916cd
Monica Gerard <mlgerardvla@gmail.com>
2020-03-11 22:02:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  string = "HAPPY NEW YEAR!"
+
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+  end
+  string
+end
+
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
